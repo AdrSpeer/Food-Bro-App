ad4f4e92ef7c87910649647dfb588d4287476373
SonoraDE <tobiastischer@outlook.de>
2024-03-13 18:02:15 +0100

    Update

--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,9 +1,39 @@
+import data from "../../../public/data";
+import React, { useEffect, useState } from "react";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import { Link } from "react-router-dom";
 import "./Searchbar.css";
+
 const Searchbar = () => {
-    return (
-        <>
-        </>
-    );
-}
- 
+  console.log(data);
+  //   useState und useEffect
+  const [searchInput, setSearchInput] = useState("");
+  const [placeholderVisible, setPlaceholderVisible] = useState(true);
+
+  //   Placeholder onFocus leeren/füllen
+  const handleFocus = () => {
+    setPlaceholderVisible(false);
+  };
+  const handleBlur = () => {
+    setPlaceholderVisible(true);
+  };
+
+  return (
+    <section className="searchbar">
+      <div className="searchbox">
+        <SearchRoundedIcon />
+        <input
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          placeholder={placeholderVisible ? "Search" : ""}
+          type="text"
+          name="searchbar"
+        />
+      </div>
+    </section>
+  );
+};
+
 export default Searchbar;
