24255ea0f462fca0b56e6918fa489c67e2d7e542
SonoraDE <tobiastischer@outlook.de>
2024-03-14 11:47:05 +0100

    Profile-Page DONE

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -18,10 +18,41 @@ const Profile = () => {
             </div> 
             <div className="balken"></div>
             <div className="profile-info">
-                <img src="#" alt="PROFILE PICTURE" />
-                <p>Vorname: {randomProfile.vorname}</p>
-                <p>Nachname: {randomProfile.nachname}</p>
-                <p>Adresse: {randomProfile.strasse}, {randomProfile.postleitzahl} {randomProfile.wohnort}</p>
+                <div className="img-flex">
+                    <img className="profile-img" src={randomProfile.profile_img} alt="PROFILE PICTURE" />
+                </div>
+                <br />
+                <br />
+                <div className="info-cont">
+                    <p className="info-title">Vorname</p>
+                    <p className="info-text">{randomProfile.vorname}</p>
+                </div>
+                <br />
+                <div className="info-cont">
+                    <p className="info-title">Nachname</p>
+                    <p className="info-text">{randomProfile.nachname}</p>
+                </div>
+                <br />
+                <div className="info-cont">
+                    <p className="info-title">Straße</p>
+                    <p className="info-text">{randomProfile.strasse}</p>
+                </div>
+                <br />
+                <div className="info-cont">
+                    <p className="info-title">Postleitzahl</p>
+                    <p className="info-text">{randomProfile.postleitzahl}</p>  
+                </div>
+                <br />
+                <div className="info-cont">
+                    <p className="info-title">Wohnort</p>
+                    <p className="info-text">{randomProfile.wohnort}</p>
+                </div>
+                <br />
+                <div className="info-cont">
+                    <p className="info-title">E-Mail Adressse</p>
+                    <p className="info-text">{randomProfile.email}</p>
+                </div>
+                <br />
             </div>
             <Footer/>
         </section>
