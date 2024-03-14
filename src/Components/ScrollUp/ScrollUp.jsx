0a6a964e2a5fbabcea5e684b1cf3133500c1fa1c
SonoraDE <tobiastischer@outlook.de>
2024-03-14 15:31:02 +0100

    Update

---
+++ src/Components/ScrollUp/ScrollUp.jsx
@@ -0,0 +1,12 @@
+import "./ScrollUp.css";
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+
+const ScrollUp = () => {
+    return (
+        <section className="scroll-up">
+            <ArrowDropUpIcon/>
+        </section>
+    );
+}
+ 
+export default ScrollUp;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Categories from "../../Components/Categories/Categories";
 import FoodCards from "../../Components/FoodCards/FoodCards";
 import Footer from "../../Components/Footer/Footer";
+import ScrollUp from "../../Components/ScrollUp/ScrollUp";
 import Searchbar from "../../Components/Searchbar/Searchbar";
 import "./Home.css";
 
