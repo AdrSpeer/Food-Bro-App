3059c4d2535dd7d0290d868a6b0c977b323e31fa
AdrSpeer <adn.speer@gmail.com>
2024-03-16 11:06:52 +0100

    changed foodcards

--- src/Components/FoodCards/FoodCards.jsx
+++ src/Components/FoodCards/FoodCards.jsx
@@ -4,20 +4,20 @@ import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRou
 import StarIcon from "@mui/icons-material/Star";
 import { Link } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { FavoriteContext } from "../../Context/Context";
 
 const FoodCards = (props) => {
-
-  
   const [filteredItems, setFilteredItems] = useState(data);
 
   useEffect(() => {
-    if(props.activeCategory.length === 0) {
-        setFilteredItems(data); 
+    if (props.activeCategory.length === 0) {
+      setFilteredItems(data);
     } else {
-      const items = data.filter(item => item.category === props.activeCategory);
-      setFilteredItems(items); 
+      const items = data.filter(
+        (item) => item.category === props.activeCategory
+      );
+      setFilteredItems(items);
     }
   }, [props.activeCategory]);
 
@@ -25,38 +25,45 @@ const FoodCards = (props) => {
 
   const addFavorite = (item) => {
     console.log(favoriteItems);
-    if(favoriteItems.some(itemdata => item.id === itemdata.id)) {
-      alert("Dieses Produkt befindet sich bereits in deiner Favoritenliste.")
+    if (favoriteItems.some((itemdata) => item.id === itemdata.id)) {
+      alert("Dieses Produkt befindet sich bereits in deiner Favoritenliste.");
     } else {
-      setFavoriteItems([...favoriteItems, item])
+      setFavoriteItems([...favoriteItems, item]);
     }
   };
 
   return (
     <section className="foodcard">
       {filteredItems.map((allData) => (
-        <Link to={`/product/${allData.id}`} key={allData.id}>
-          <div className="foodcard-box" >
-            <div className="foodcard-rate">
-              <StarIcon style={{ fill: "#ffc700" }} />
-              <p>{allData.rating}</p>
-            </div>
-            <div className="foodcard-img">
-              <img src={allData.image} alt={allData.name} />
-            </div>
-            <div className="foodcard-content">
-              <h2>{allData.name}</h2>
-              <p>{allData.shortdesc}</p>
-            </div>
-            <div className="foodcard-price">
-              <p>${allData.price}</p>
-              <AddCircleOutlineRoundedIcon />
-            </div>
-            <div className="favo-icon">
-              <FavoriteIcon onClick={() => addFavorite(allData)} className={`${favoriteItems.some(itemdata => allData.id === itemdata.id) ? "favo-active" : "" }`}/>
-            </div>
+        <div key={allData.id} className="foodcard-box">
+          <div className="foodcard-rate">
+            <StarIcon style={{ fill: "#ffc700" }} />
+            <p>{allData.rating}</p>
           </div>
+          <Link to={`/product/${allData.id}`}>
+            <div className="foodcard-content">
+              <div className="foodcard-img">
+                <img src={allData.image} alt={allData.name} />
+              </div>
+              <h2>{allData.name}</h2>
+            </div>
           </Link>
+          <p className="foodcard-shortdesc">{allData.shortdesc}</p>
+          <div className="foodcard-price">
+            <p>${allData.price}</p>
+            <AddCircleOutlineRoundedIcon />
+          </div>
+          <div className="favo-icon">
+            <FavoriteIcon
+              onClick={() => addFavorite(allData)}
+              className={`${
+                favoriteItems.some((itemdata) => allData.id === itemdata.id)
+                  ? "favo-active"
+                  : ""
+              }`}
+            />
+          </div>
+        </div>
       ))}
     </section>
   );
