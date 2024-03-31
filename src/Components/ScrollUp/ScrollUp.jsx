eeb28d58b9771843380de3a4c955fc1b3ecfccd9
SonoraDE <tobiastischer@outlook.de>
2024-03-31 16:18:01 +0200

    Push

--- src/Components/ScrollUp/ScrollUp.jsx
+++ src/Components/ScrollUp/ScrollUp.jsx
@@ -1,11 +1,7 @@
-import "./ScrollUp.css";
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-
 const ScrollUp = () => {
     return (
-        <section className="scroll-up">
-            <ArrowDropUpIcon/>
-        </section>
+        <>
+        </>
     );
 }
  
